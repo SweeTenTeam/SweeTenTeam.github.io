8af3844538abb4c3ae9652cdf44f1956d8c05560
Matteo Campagnaro <matteo.campagnaro.1@studenti.unipd.it>
2025-04-24 17:30:29 +0200

    update glossario

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
 import csv
 from collections import defaultdict
 
-# Inserisci il percorso corretto del file CSV qui
-csv_file_path = "gloss.csv"
+csv_file_path = "gloss (1).csv"
 output_html_path = "glossario_generato.html"
-
-# Dizionario per raccogliere i termini per lettera iniziale
 terms_by_letter = defaultdict(list)
 
-# Legge il file CSV
 with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -17,7 +13,6 @@ with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
         first_letter = term[0].upper()
         terms_by_letter[first_letter].append((term, definition))
 
-# Genera l'HTML
 html_output = ""
 for letter in sorted(terms_by_letter):
     html_output += f'<div id="{letter}">\n\t<h3>{letter}</h3>\n'
@@ -28,7 +23,6 @@ for letter in sorted(terms_by_letter):
         html_output += f'\t</div>\n'
     html_output += '</div>\n\n'
 
-# Salva l'output in un file HTML
 with open(output_html_path, "w", encoding="utf-8") as output_file:
     output_file.write(html_output)
 
